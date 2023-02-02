84a3e6c93ae5f2e3677a70ef885afc5772ff70b3
hacktran <01216603581abc@gmial.com>
2023-02-02 21:59:38 +0700

    push code

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = '';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng Sinh Nhật 03-02-2002';
+const titleCard = "Happy Birthday";
+const contentCard = 'Mừng ngày mi chào đời 21 năm trước! Gửi tới mi những lời chúc tốt đẹp nhất. Chúc cho những dự định trong tương lai sắp đến luôn thành công, cuộc sống luôn hạnh phúc, vui tươi. Sớm tìm được mảnh ghép còn thiếu sót, sớm tối bên kề, cùng nhau nếm đủ dư vị của cuộc sống!';
+
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = './beo.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
